e8abb2cd654ca049a893633fb79d7ff9f2f0b4df
serstep <myserstep@mail.ru>
2016-12-21 18:44:52 +0200

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,42 @@
+#!/usr/local/bin/python3.5
+import re, sys
 
 
-def get_password_strength(password):
-    pass
+def read_dict(filepath):
+    with open(filepath) as dictionary :
+        return dictionary.read()
+
+def dict_exist_rate(password, dictionary):
+    if dictionary is None:
+        return 0
+    return 2 if re.search(password, dictionary, flags=re.IGNORECASE) is None else 0
+
+
+def is_password_valid(password):
+    return True if re.search(r"[^@#$%^&*\dA-z]", password) is None else False
+
+
+def get_password_strength(password, dictionary):
+    password_length = len(password)
+    password_rate = int(password_length >= 8) + 2 * int(password_length >= 12)
+    password_rate += int(password.lower() != password and password.upper() != password)
+    password_rate += 1 if not re.search(r"[\d]", password) is None else 0
+    password_rate += 1 if not re.search(r"[@#$%^&*]", password) is None else 0
+    password_rate += 2 if re.search(r"((19)|(20))\d\d", password) is None else 0
+    password_rate += dict_exist_rate(password, dictionary)
+
+    return password_rate
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        dictionary = read_dict(sys.argv[1])
+    except:
+        dictionary = None
+        print("Словарь не задан")
+
+    password = input("Введите пароль:\n")
+    if is_password_valid(password):
+        print("Оценка вашего пароля:", get_password_strength(password, dictionary), "из 10")
+    else:
+        print("Пароль содержит запрещенные символы.")
